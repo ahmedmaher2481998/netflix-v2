9c0d74441d682f86aa19f252ca04b480e9c71068
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2022-08-26 16:13:50 +0200

    added : jumbo container

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import JumboTron from "./components/jumbotron";
-import jumbotronData from "./fixtures/jumbo.json";
+import JumbotronContainer from "./containers/Jumbotron";
 
 function App() {
 	return (
-		<JumboTron.Container>
-			{jumbotronData.map((item) => (
-				<JumboTron direction={item.direction} key={item.id}>
-					<JumboTron.Pane>
-						<JumboTron.Title>{item.title}</JumboTron.Title>
-						<JumboTron.Subtitle>{item.subTitle}</JumboTron.Subtitle>
-					</JumboTron.Pane>
-					<JumboTron.Pane>
-						<JumboTron.Image src={item.image} alt={item.alt} />
-					</JumboTron.Pane>
-				</JumboTron>
-			))}
-			<JumboTron></JumboTron>
-		</JumboTron.Container>
+		<>
+			<JumbotronContainer />
+		</>
 	);
 }
 
