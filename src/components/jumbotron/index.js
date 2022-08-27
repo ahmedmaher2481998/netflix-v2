5bd29837155c90c2c05228319c97341bc8d3e97a
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2022-08-27 11:27:00 +0200

    added : eslint / preittier

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -9,23 +9,23 @@ function JumboTron({ children, direction = 'row', ...restProps }) {
   );
 }
 
-JumboTron.Container = function ({ children, ...restProps }) {
+JumboTron.Container = function JumboTronContainer({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 };
 
-JumboTron.Pane = function ({ children, ...restProps }) {
+JumboTron.Pane = function JumboTronPane({ children, ...restProps }) {
   return <Pane {...restProps}>{children}</Pane>;
 };
 
-JumboTron.Title = function ({ children, ...restProps }) {
+JumboTron.Title = function JumboTronTitle({ children, ...restProps }) {
   return <Title {...restProps}>{children}</Title>;
 };
 
-JumboTron.Subtitle = function ({ children, ...restProps }) {
+JumboTron.Subtitle = function JumboTronSubtitle({ children, ...restProps }) {
   return <Subtitle {...restProps}>{children}</Subtitle>;
 };
 
-JumboTron.Image = function ({ ...restProps }) {
+JumboTron.Image = function JumboTronImage({ ...restProps }) {
   return <Image {...restProps} />;
 };
 
