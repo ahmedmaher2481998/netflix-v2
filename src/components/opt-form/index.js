b8e1517dbaf1fab7a63e11c3f74c1abf48caf829
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2022-08-28 12:31:52 +0200

    added : opt from boilerpalte

---
+++ src/components/opt-form/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Input, Button, Break, Text } from './styles/opt-form';
+const OptForm = ({ children, ...restProps }) => {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+OptForm.Input = function OptFromInput({ children, ...restProps }) {
+  return <Input {...restProps}>{children}</Input>;
+};
+
+OptForm.Button = function OptFromButton({ children, ...restProps }) {
+  return (
+    <Button {...restProps}>
+      {children}
+      <img src="/images/icons/chevron-right.png" alt="Try Now" />
+    </Button>
+  );
+};
+
+OptForm.Text = function OptFromText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+OptForm.Break = function OptFromBreak({ children, ...restProps }) {
+  return <Break {...restProps}>{children}</Break>;
+};
+
+export default OptForm;
