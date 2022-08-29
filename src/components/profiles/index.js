e5509a2bd63e4e531f1d79698c634ee527c29d7d
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-08-29 19:57:43 +0200

    done profile component

--- src/components/profiles/index.js
+++ src/components/profiles/index.js
@@ -8,5 +8,18 @@ const Profile = ({ children, ...rest }) => {
 Profile.Title = function ProfileTitle({ children, ...rest }) {
   return <Title {...rest}>{children}</Title>;
 };
+Profile.List = function ProfileList({ children, ...rest }) {
+  return <List {...rest}>{children}</List>;
+};
+
+Profile.Name = function ProfileName({ children, ...rest }) {
+  return <Name {...rest}>{children}</Name>;
+};
+Profile.User = function ProfileUser({ children, ...rest }) {
+  return <Item {...rest}>{children}</Item>;
+};
+Profile.Picture = function ProfilePicture({ src, ...rest }) {
+  return <Picture {...rest} src={src ? `/images/users/${src}.png` : '/images/misc/loading.gif'} />;
+};
 
 export default Profile;
