f7cd0010860cc8c796f9f48009920fb7b7c6fd45
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2022-08-29 14:01:17 +0200

    add : routing edit 2

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path={routes.Home} element={user ? <Navigate to={routes.Browse} /> : <Home />} />
-
         <Route path={routes.Browse} element={user ? <Browse /> : <Navigate to={routes.SignIn} />} />
-
         <Route path={routes.SignIn} element={user ? <Navigate to={routes.Browse} /> : <SignIn />} />
         <Route path={routes.SignUp} element={user ? <Navigate to={routes.Browse} /> : <SignUp />} />
       </Routes>
