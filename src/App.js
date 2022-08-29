47382ac838a44f1a7adfba90e8ae67ab7fae0619
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2022-08-29 13:54:52 +0200

    rm router helpers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import * as routes from './constants/routes';
 // { Browse, Home, SignIn, SignUp }
 import { Browse, Home, SignIn, SignUp } from './pages/';
+
 function App() {
+  const user = { hi: 'ahmed' };
+  // const user = null;
   return (
     <Router>
       <Routes>
-        <Route path={routes.Home} element={<Home />} />
-        <Route path={routes.Browse} element={<Browse />} />
+        <Route path={routes.Home} element={user ? <Navigate to={routes.Browse} /> : <Home />} />
+
+        <Route path={routes.Browse} element={user ? <Browse /> : <Navigate to={routes.SignIn} />} />
+
         <Route path={routes.SignIn} element={<SignIn />} />
         <Route path={routes.SignUp} element={<SignUp />} />
       </Routes>
