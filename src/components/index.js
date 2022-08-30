5a60a99a3f320ef9d30241e8ac67b2a71968f141
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-08-30 12:19:17 +0200

    reinit working on the browse page

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,5 @@ export { default as Accordion } from './accordion/';
 export { default as OptForm } from './opt-form/';
 export { default as Header } from './Header/';
 export { default as Feature } from './Feature';
-
+export { default as Profiles } from './profiles';
 export { default as Form } from './form';
---
+++ src/containers/profiles.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Profiles } from '../components';
+import * as routes from '../constants/routes';
+
+const ProfileContainer = () => {
+  return <div>ProfileContainer</div>;
+};
+
+export default ProfileContainer;
