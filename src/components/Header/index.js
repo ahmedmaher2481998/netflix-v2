c602ad9d825a6216c9e8099f7cc8eded86106f29
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-08-30 19:20:25 +0200

    styled : edited container style in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,6 @@ import {
   ButtonLink,
   Frame,
   Logo,
-  Container,
   Dropdown,
   Feature,
   FeatureCallOut,
--- src/components/Header/styles/Header.js
+++ src/components/Header/styles/Header.js
@@ -16,19 +16,7 @@ export const Background = styled.div`
     ${({ hideOnSmallScreen }) => hideOnSmallScreen && `background: none;`}
   }
 `;
-export const Container = styled.div`
-  display: flex;
-  margin: 0 56px;
-  height: 100px;
-  justify-content: space-between;
-  align-items: center;
-  a {
-    display: flex;
-  }
-  @media (max-width: 1000px) {
-    margin: 0 30px;
-  }
-`;
+
 export const Logo = styled.svg`
   height: 36px;
   width: 134px;
