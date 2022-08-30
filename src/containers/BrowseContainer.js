7a3d3500fa8033d0aa94f6bef0d5fd6a85d5cb64
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-08-30 13:16:15 +0200

    select profile from browse container

--- src/containers/BrowseContainer.js
+++ src/containers/BrowseContainer.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import { useContext, useEffect, useState } from 'react';
 import SelectProfileContainer from './Profiles';
+import { FireBaseContext } from '../context/firebase';
 
 const BrowseContainer = () => {
-  return <SelectProfileContainer />;
+  const { auth } = useContext(FireBaseContext);
+  const user = auth.currentUser || {};
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    console.log(profile);
+    const t = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return clearTimeout(t);
+  }, [profile.displayName]);
+
+  return <SelectProfileContainer user={user} setProfile={setProfile} />;
 };
 
 export default BrowseContainer;
--- src/containers/Profiles.js
+++ src/containers/Profiles.js
@@ -17,6 +17,22 @@ const SelectProfileContainer = ({ user, setProfile }) => {
           />
         </Header.Frame>
       </Header>
+      <Profiles>
+        <Profiles.Title>How is watching ? </Profiles.Title>
+        <Profiles.List>
+          <Profiles.User
+            onClick={() => {
+              console.log('clicked');
+              setProfile(() => ({
+                displayName: user.displayName,
+                photoURL: user.photoURL
+              }));
+            }}>
+            <Profiles.Picture src={user.photoURL} />
+            <Profiles.Name>{user.displayName}</Profiles.Name>
+          </Profiles.User>
+        </Profiles.List>
+      </Profiles>
     </>
   );
 };
