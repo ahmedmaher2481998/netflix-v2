490715d1066b399d1c7d63a2339840974e2a9a41
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-08-31 22:49:01 +0200

    styling card component

--- src/components/card/styles/cards.js
+++ src/components/card/styles/cards.js
@@ -8,6 +8,7 @@ export const Title = styled.p`
   margin-right: 56px;
   margin-top: 0;
 `;
+
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -30,11 +31,17 @@ export const SubTitle = styled.p`
 `;
 
 export const Item = styled.div``;
+
 export const Image = styled.img``;
+
 export const Feature = styled.div``;
+
 export const FeatureTitle = styled.h2``;
+
 export const FeatureText = styled.p``;
+
 export const FeatureClose = styled.div``;
+
 export const Group = styled.div`
   display: flex;
   flex-direction: ${({ flexDirection }) => (flexDirection === 'row' ? 'row' : 'column')};
@@ -56,10 +63,39 @@ export const Text = styled.p`
   display: none;
   line-height: normal;
 `;
+
 export const Entities = styled.div`
   display: flex;
   flex-direction: row;
 `;
-export const Meta = styled.div``;
-export const Maturity = styled.div``;
-export const Content = styled.div``;
+
+export const Meta = styled.div`
+  display: none;
+  position: absolute;
+  bottom: 0;
+  padding: 10px;
+  background-color: #0000008f;
+`;
+
+export const Maturity = styled.div`
+  background-color: ${(rating) => (rating >= 15 ? 'red' : 'green')};
+  border-radius: 15px;
+  width: 28px;
+  line-height: 28px;
+  text-align: center;
+  color: white;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  font-size: 12px;
+  margin-right: 10px;
+`;
+
+export const Content = styled.div`
+  margin: 24px;
+  max-width: 500px;
+  line-height: normal;
+  @media (max-width: 1000px) {
+    margin: 30px;
+    max-width: none;
+  }
+`;
