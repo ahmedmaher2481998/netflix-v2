4b1b8fd92ec432d9186dcb30e7639ed68bc205d8
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-08-31 22:27:18 +0200

    styled card title

--- src/components/card/styles/cards.js
+++ src/components/card/styles/cards.js
@@ -1,7 +1,14 @@
 import styled from 'styled-components/macro';
 
 export const Container = styled.div``;
-export const Title = styled.p``;
+export const Title = styled.p`
+  font-size: 24px;
+  color: #e5e5e5;
+  font-weight: bold;
+  margin-left: 56px;
+  margin-right: 56px;
+  margin-top: 0;
+`;
 export const SubTitle = styled.p``;
 export const Item = styled.div``;
 export const Image = styled.img``;
