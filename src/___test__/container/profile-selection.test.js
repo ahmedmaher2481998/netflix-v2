6181adae8600a066dea61783b2b941086fc78b21
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-09-01 22:53:01 +0200

    test selection filter

---
+++ src/___test__/container/profile-selection.test.js
@@ -0,0 +1,41 @@
+import selectionFilter from '../../utils/selection-filter';
+
+test('selection filter works as intended ', () => {
+  const series = [
+    {
+      id: 'x1-series',
+      title: 'ninja cat movie',
+      description:
+        'An exploration of big cat breeding and its bizarre underworld, populated by eccentric characters.',
+      genre: 'comedies',
+      maturity: '12',
+      slug: 'ninja-cat'
+    }
+  ];
+  const films = [
+    {
+      id: 'x1-films',
+      title: 'The Prestige',
+      description: 'Great film...',
+      genre: 'drama',
+      maturity: '15',
+      slug: 'the-prestige'
+    }
+  ];
+
+  const slides = selectionFilter({ films, series });
+
+  expect(slides.films[0].title).toBe('Drama');
+  expect(slides.films[0].data[0].description).toBe('Great film...');
+  expect(slides.films[0].data[0].genre).toBe('drama');
+  expect(slides.films[0].data[0].maturity).toBe('15');
+  expect(slides.films[0].data[0].slug).toBe('the-prestige');
+
+  expect(slides.series[1].title).toBe('Comedies');
+  expect(slides.series[1].data[0].description).toBe(
+    'An exploration of big cat breeding and its bizarre underworld, populated by eccentric characters.'
+  );
+  expect(slides.series[1].data[0].genre).toBe('comedies');
+  expect(slides.series[1].data[0].maturity).toBe('12');
+  expect(slides.series[1].data[0].slug).toBe('ninja-cat');
+});
--- src/___test__/utils/selection-filter.test.js
+++ src/___test__/utils/selection-filter.test.js
@@ -1,11 +1,11 @@
 import { render, fireEvent } from '@testing-library/react';
-import { SelectProfileContainer } from '../../containers/Profiles';
+import SelectProfileContainer from '../../containers/Profiles';
 
 jest.mock('react-router-dom');
 
 describe('<SelectProfileContainer/>', () => {
   it('renders the <SelectProfileContainer/>', () => {
-    const user = { displayName: 'ahmed maher', photoURL: 'images/user/4.png' };
+    const user = { displayName: 'ahmed maher', photoURL: 'profile.png' };
     const setProfile = jest.fn();
     const { getByTestId } = render(<SelectProfileContainer user={user} setProfile={setProfile} />);
 
