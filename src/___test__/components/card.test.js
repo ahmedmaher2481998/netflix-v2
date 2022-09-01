17dffd96d1cb3bc8eb6d59366e3af56924db8b5b
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-09-01 19:01:38 +0200

    test card

--- src/___test__/components/card.test.js
+++ src/___test__/components/card.test.js
@@ -1,5 +1,5 @@
-import { render } from '@testing-library/react';
-import { Card } from '../../components';
+import { render, fireEvent } from '@testing-library/react';
+import { Card, Player } from '../../components';
 const category = 'series';
 const slideRow = [
   {
@@ -33,7 +33,7 @@ const slideRow = [
 ];
 describe('<Card/>', () => {
   it('renders <Card/> with populated data ', () => {
-    const { container, queryByText, getByText, getByAltText, getByTestId } = render(
+    const { container, queryByText, getByAltText } = render(
       <Card.Group>
         {slideRow.map((slideItem, i) => (
           <Card key={`${category}${i}-${slideItem?.title?.toLowerCase()}`}>
@@ -52,10 +52,73 @@ describe('<Card/>', () => {
                 </Card.Item>
               ))}
             </Card.Entities>
-            <Card.Feature category={category}></Card.Feature>
+            <Card.Feature category={category}>
+              <Player>
+                <Player.Button />
+                <Player.Video src={'/videos/bunny.mp4'} />
+              </Player>
+            </Card.Feature>
           </Card>
         ))}
       </Card.Group>
     );
+    expect(queryByText(slideRow[0].title)).toBeTruthy();
+    expect(queryByText(slideRow[0].data[0].title)).toBeTruthy();
+    expect(getByAltText(slideRow[0].data[0].slug)).toBeTruthy();
+    expect(queryByText(slideRow[0].data[0].description)).toBeTruthy();
+
+    expect(queryByText(slideRow[0].title)).toBeTruthy();
+    expect(queryByText(slideRow[0].data[1].title)).toBeTruthy();
+    expect(getByAltText(slideRow[0].data[1].slug)).toBeTruthy();
+    expect(queryByText(slideRow[0].data[1].description)).toBeTruthy();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('toggles the feature upon clicking  a card item', () => {
+    const { queryByText, getByAltText, container, getByTestId } = render(
+      <Card.Group>
+        {slideRow.map((slideItem, i) => (
+          <Card key={`${category}${i}-${slideItem?.title?.toLowerCase()}`}>
+            <Card.Title>{slideItem?.title}</Card.Title>
+            <Card.Entities>
+              {slideItem?.data?.map((item) => (
+                <Card.Item item={item} key={item.docId} data-testid={`${item.slug}-item-feature`}>
+                  <Card.Image
+                    src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
+                    alt={item.slug}
+                  />
+                  <Card.Meta>
+                    <Card.SubTitle>{item.title}</Card.SubTitle>
+                    <Card.Text>{item.description}</Card.Text>
+                  </Card.Meta>
+                </Card.Item>
+              ))}
+            </Card.Entities>
+            <Card.Feature category={category}>
+              <Player>
+                <Player.Button />
+                <Player.Video src={'/videos/bunny.mp4'} />
+              </Player>
+            </Card.Feature>
+          </Card>
+        ))}
+      </Card.Group>
+    );
+
+    expect(queryByText('18')).toBeFalsy();
+    fireEvent.click(getByTestId('tiger-king-item-feature'));
+    expect(queryByText('18')).toBeTruthy();
+
+    fireEvent.click(getByAltText('Close'));
+    expect(queryByText('18')).toBeFalsy();
+
+    expect(queryByText('PG')).toBeFalsy();
+    fireEvent.click(getByTestId('juno-item-feature'));
+    expect(queryByText('PG')).toBeTruthy();
+
+    fireEvent.click(getByAltText('Close'));
+    expect(queryByText('PG')).toBeFalsy();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
