59839730270159a267e3e9fd71adfb12bd4647ee
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-09-01 19:45:10 +0200

    test fomr -2

--- src/___test__/components/form.test.js
+++ src/___test__/components/form.test.js
@@ -1,40 +1,52 @@
-import { getByTestId, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Form } from '../../components';
 jest.mock('react-router-dom');
 describe('<From/>', () => {
   it('renders the form with populated data', () => {
-    const { container, getByPlaceholderText, getByText, queryByText } = render(
+    const { container, getByText, getByPlaceholderText } = render(
       <Form>
-        <Form.Title>Sign In Now.</Form.Title>
+        <Form.Title>Sign In Now</Form.Title>
 
         <Form.Base>
-          <Form.Input onChange={() => {}} type="email" placeholder="Email Address" />
-          <Form.Input onChange={() => {}} type="password" placeholder="Password" />
+          <Form.Input placeholder="Email address" onChange={() => {}} />
+          <Form.Input type="password" placeholder="Password" onChange={() => {}} />
           <Form.Submit type="submit" disabled>
-            {' '}
-            Sign In{' '}
+            Sign In
           </Form.Submit>
-          <Form.Text>
-            New to Netflix? <Form.Link to={'/signup'}> Sign up now.</Form.Link>
-          </Form.Text>
-          <Form.SmallText>
-            {" This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more."}
-          </Form.SmallText>
         </Form.Base>
+
+        <Form.Text>
+          New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+        </Form.Text>
+        <Form.TextSmall>
+          This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+        </Form.TextSmall>
       </Form>
     );
-    expect(queryByText('Sign In Now.')).toBeTruthy();
-    expect(queryByText('Sign In')).toBeTruthy();
-    expect(queryByText('New to Netflix?')).toBeTruthy();
 
     expect(
       getByText(
         "This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more."
       )
     ).toBeTruthy();
-    expect(queryByText('Sign In').disabled).toBeTruthy();
-    expect(getByPlaceholderText('Email Address').disabled).toBeTruthy();
-    expect(getByPlaceholderText('Password').disabled).toBeTruthy();
+    expect(getByText('Sign In Now')).toBeTruthy();
+    expect(getByText('Sign In')).toBeTruthy();
+    expect(getByText('Sign In').disabled).toBeTruthy();
+    expect(getByPlaceholderText('Email address')).toBeTruthy();
+    expect(getByPlaceholderText('Password')).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('renders the error component', () => {
+    const { container, getByText, queryByText } = render(
+      <Form>
+        <Form.Error>Your email address is already being used</Form.Error>
+        <Form.Submit type="submit">Sign In</Form.Submit>
+      </Form>
+    );
+
+    expect(getByText('Your email address is already being used')).toBeTruthy();
+    expect(queryByText('Sign In').disabled).toBeFalsy();
     expect(container.firstChild).toMatchSnapshot();
   });
 });
